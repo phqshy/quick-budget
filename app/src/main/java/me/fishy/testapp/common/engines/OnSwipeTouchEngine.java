2f4794bbbffdff5d8f6646d9981eca149811f883
phqshy <87027056+phqshy@users.noreply.github.com>
2022-04-26 09:03:32 -0400

    Tweaked velocity and distance for swipes

--- app/src/main/java/me/fishy/testapp/common/engines/OnSwipeTouchEngine.java
+++ app/src/main/java/me/fishy/testapp/common/engines/OnSwipeTouchEngine.java
@@ -31,8 +31,8 @@ public class OnSwipeTouchEngine implements View.OnTouchListener {
 
     private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
 
-        private static final int SWIPE_DISTANCE_THRESHOLD = 70;
-        private static final int SWIPE_VELOCITY_THRESHOLD = 70;
+        private static final int SWIPE_DISTANCE_THRESHOLD = 20;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 50;
 
         @Override
         public boolean onDown(MotionEvent e) {
